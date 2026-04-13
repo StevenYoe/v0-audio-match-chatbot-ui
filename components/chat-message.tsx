8274df356523e816209f29b0755095ac37a0a3a0
StevenYoe <stevenyoe80@gmail.com>
2026-04-13 10:38:41 +0700

    remove empty recommended products section from chat messages

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -3,7 +3,6 @@
 import { motion } from 'framer-motion';
 import { Message } from '@/lib/store';
 import { formatNumbersInText } from '@/lib/utils';
-import InlineRecommendations from './inline-recommendations';
 import ReactMarkdown from 'react-markdown';
 
 interface ChatMessageProps {
@@ -41,11 +40,6 @@ export default function ChatMessage({ message, index }: ChatMessageProps) {
         <span className="text-xs text-muted-foreground mt-2 block opacity-60">
           {message.timestamp.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
         </span>
-
-        {/* Display inline recommendations if available */}
-        {!isUser && message.recommendations && message.recommendations.length > 0 && (
-          <InlineRecommendations recommendations={message.recommendations} />
-        )}
       </div>
     </motion.div>
   );
